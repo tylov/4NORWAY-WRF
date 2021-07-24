91213388c5d1fbf4bc1512b6f834961997da66a6
tylo <tylo@norceresearch.no>
2021-07-24 21:46:08 +0200

    First commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+1) On Nird, run: python Extract_Vars_RF.py 1984
+2) On Fram with bignode: Hybrid_To_PRES_new.ncl.
+   Remember to change year inside the script. Queue with: submit2queue.sh (includes loading modules)
+
+3) To get Jan 1. 00:00, run the year before as well, i.e. I have run 1983 and 1994, files are ready for you.
---
+++ submit2queue.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#  Give the job a name
+#SBATCH --job-name=IM_files
+#
+#  Specify the project the job belongs to
+#SBATCH --account=nn9560k
+#SBATCH --time=1:20:00
+#SBATCH --partition=bigmem
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=170G
+
+###SBATCH --nodes=1 --ntasks-per-node=1
+#  Set OMP_NUM_THREADS
+###SBATCH --cpus-per-task=1
+
+## Set email notifictions
+###SBATCH --mail-user=marie.pontoppidan@uni.no
+#SBATCH --mail-user=tylo@norceresearch.no
+#SBATCH --mail-type=ALL
+
+set -o errexit # Make bash exit on any error
+set -o nounset # Treat unset variables as errors
+
+set -xve
+
+module purge 
+module load NCL/6.5.0-intel-2018a
+# module load NCL/6.6.2-intel-2019b # betzy - not working with bigmem
+
+WDIR=/cluster/work/users/$USER/IM_NorESM
+cd $WDIR
+ncl Hybrid_To_Pres_new.ncl CASE=\"$1\" outROOT=\"$2\" > log-Hybrid_To_Pres_new-$1.txt
